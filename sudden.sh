5069aa18a78f76539ec93405dbfcca35755aa03f
hun0219 <gsw98000@gmail.com>
2024-07-11 10:08:40 +0900

    dev deploy

--- sudden.sh
+++ sudden.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 FILE=~/tmp/_SUDDEN
+#MODEL=KIA
 
 while true
 do
-	if [ -f "$FILE" ]; then
-		echo "sudden"
+	if [[ -f "$FILE" ]]; then
+	#	echo "check $FILE"
+		echo "sudden!!!"
+	#	sleep 3
 	else
 		echo "error"
+		date
 	fi
-
-	sleep 3
+	sleep 1
 done
+
+
+#while true
+#do
+#        echo "check $MODEL"
+#        sleep 3
+#done
