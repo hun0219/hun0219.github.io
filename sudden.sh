8b21efa031fb8116e02393b170ac51934795e3d0
hun0219 <94940460+hun0219@users.noreply.github.com>
2024-07-11 15:16:57 +0900

    Update sudden.sh

--- sudden.sh
+++ sudden.sh
@@ -9,14 +9,14 @@ FILE=~/tmp/_SUDDEN
 
 while true
 do
+	DATE=$(date +"%Y%m%d %H:%M:%S")
 	if [[ -f "$FILE" ]]; then
 	#	echo "check $FILE"
-		echo "sudden!!!"
+		echo "$DATE,SUDDEN!!!"
 	#	sleep 3
 	else
 		figlet KIA
 	#	echo "error"
-	DATE=$(date +"%Y%m%d %H:%M:%S")
 		echo "$DATE,SUDDEN!!!"
 	fi
 	sleep 3
